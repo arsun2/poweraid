181606b027c9f8b97f28e5e428cde01803909a4d
Lina Sie <linahsie@gmail.com>
2018-10-20 01:51:04 -0400

    changed manifest

--- bundle.js
+++ bundle.js
@@ -16415,14 +16415,14 @@ function extend() {
 },{}],66:[function(require,module,exports){
 var erBase = require("eventregistry");
 
-var er = new erBase.EventRegistry({apiKey: "0cfa9b02-9e6f-4675-a59f-8a9722e52415"});
+/*var er = new erBase.EventRegistry({apiKey: "0cfa9b02-9e6f-4675-a59f-8a9722e52415"});
 
 er.getConceptUri("George Clooney").then((conceptUri) => {
     var q = new erBase.QueryArticlesIter(er, {conceptUri: conceptUri, sortBy: "date"});
     q.execQuery((item) => {
         console.info(item);
     })
-});
+});*/
 
 },{"eventregistry":133}],67:[function(require,module,exports){
 'use strict';
--- news.js
+++ news.js
@@ -2,9 +2,10 @@ var erBase = require("eventregistry");
 
 var er = new erBase.EventRegistry({apiKey: "0cfa9b02-9e6f-4675-a59f-8a9722e52415"});
 
+console.log("amen");
 er.getConceptUri("George Clooney").then((conceptUri) => {
     var q = new erBase.QueryArticlesIter(er, {conceptUri: conceptUri, sortBy: "date"});
     q.execQuery((item) => {
-        console.info(item);
+        console.log(item);
     })
 });
