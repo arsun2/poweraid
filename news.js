2195671e9eded6ce3ad69e4e3c4225b08596a66a
Lina Sie <linahsie@gmail.com>
2018-10-20 17:20:19 -0400

    basic ui

--- news.js
+++ news.js
@@ -1,37 +1,21 @@
-console.log("hello");
-/*
-var fs = require('fs');
-var erBase = require("eventregistry");
-
-var er = new erBase.EventRegistry(/*{apiKey: "0cfa9b02-9e6f-4675-a59f-8a9722e52415"});
-
-console.log("hi");
-
-er.getConceptUri("George Clooney").then((conceptUri) => {
-    var q = new erBase.QueryArticlesIter(er, {conceptUri: conceptUri, sortBy: "date"});
-    q.execQuery((item) => {
-        console.log(item);
-    })
-}); */
-
 var request = require('request');
 request.get({
-    url: "http://eventregistry.org/api/v1/article/getArticles",
+    url: "http://eventregistry.org/api/v1/event/getEvents",
     qs: {
-        "resultType": "articles",
+        "resultType": "events", // ??
         "keyword": [
-                "Bitcoin",
-                "Ethereum",
-                "Litecoin"
+            "earthquake",
+            "tsunami",
+            "hurricane",
+            "flood"
         ],
-        "keywordOper": "or",
+        "keywordLoc": "title", // ??
         "lang": "eng",
-        "articlesSortBy": "date",
-        "includeArticleConcepts": true,
-        "includeArticleCategories": true,
-        "articleBodyLen": 300,
-        "articlesCount": 10,
-        "apiKey": "0cfa9b02-9e6f-4675-a59f-8a9722e52415"
+        "eventsSortBy": "date",
+        "includeEventCategories": true,
+        "eventsCount": 30, // default=50
+        "apiKey": "0cfa9b02-9e6f-4675-a59f-8a9722e52415",
+        "ignoreCategoryUri": "",
 }},
     function(error, response, body) {
         //body = JSON.parse(body);
